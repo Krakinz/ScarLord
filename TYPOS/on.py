b9602c2b348e3fbdec2f31627457e1bccd7f9782
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-04-15 18:18:06 +0300

    Add files via upload

---
+++ TYPOS/on.py
@@ -0,0 +1,22 @@
+KLAWNESS = """—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—
+
+GoodSir/Ma'am `{}` 
+
+🚀𝘛𝘦𝘭𝘦𝘨𝘳𝘢𝘮 𝘎𝘳𝘰𝘶𝘱 𝘮𝘢𝘯𝘢𝘨𝘦𝘮𝘦𝘯𝘵 𝘣𝘰𝘵 𝘸𝘪𝘵𝘩 𝘩𝘶𝘨𝘦 𝘯𝘶𝘮𝘣𝘦𝘳 𝘰𝘧 𝘮𝘰𝘥𝘶𝘭𝘦𝘴 𝘵𝘩𝘢𝘵 𝘤𝘢𝘯 𝘣𝘦 𝘶𝘴𝘦𝘥.
+🚀ʙɪɢ ʙʀᴏᴛʜᴇʀ ᴏꜰ @HVKlawBot
+🚀𝘔𝘶𝘤𝘩 𝘭𝘪𝘬𝘦 𝘮𝘺 𝘚𝘪𝘣𝘭𝘪𝘯𝘨 𝘣𝘰𝘵 [@hypevoidlab].
+
+🚀`𝘚𝘪𝘮𝘱𝘭𝘺 𝘱𝘳𝘦𝘴𝘴 𝘰𝘯` /help `𝘪𝘯 𝘱𝘳𝘪𝘷𝘢𝘵𝘦 𝘢𝘯𝘥 𝘺𝘰𝘶 𝘢𝘳𝘦 𝘨𝘰𝘰𝘥 𝘵𝘰 𝘨𝘰.`
+ 
+ 🦞DΣV MΣПƬIӨП:💻 {}
+ 
+—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—"""
+KLAW_MORE = """—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—
+
+Here Are All the Buttons you will ever need.
+ 
+ 🦞DΣV MΣПƬIӨП:
+💻 @hypevoidsoul | @hypevoidbot  
+—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—"""
+SƈαɾLσɾԃ_IMG = "https://telegra.ph/file/dbbae1c53860e42fe3ebe.jpg"
+IGNIT_KLAW = "—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—\n\nThis Group has now been added as a verified @HypeVoids SƈαɾLσɾԃ⚔️ fueled group.<b>Haven't slept since:</b> <code>{}</code>\n\n—✨••÷[  SƈαɾLσɾԃ⚔️  ]÷••✨—"
